6691c2032ec7c83c0ecf87585bec70455a5adf03
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-28 01:33:43 -0500

    feat: Show browser from pin

--- OnTheMap/Controller/MapViewController.swift
+++ OnTheMap/Controller/MapViewController.swift
@@ -116,9 +116,8 @@ extension MapViewController: MKMapViewDelegate {
 
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         if control == view.rightCalloutAccessoryView {
-            let app = UIApplication.shared
             if let toOpen = view.annotation?.subtitle! {
-                print(URL(string: toOpen)!)
+                BrowserUtil.open(viewController: self, urlString: toOpen)
             }
         }
     }
