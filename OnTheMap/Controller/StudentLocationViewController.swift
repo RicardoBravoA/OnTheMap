207f4d41dc7b4b1d74d2bd85c6fc9161f4c2bc37
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-27 21:41:33 -0500

    feat: Add data model

---
+++ OnTheMap/Controller/StudentLocationViewController.swift
@@ -0,0 +1,8 @@
+//
+//  StudentLocationViewController.swift
+//  OnTheMap
+//
+//  Created by Ricardo Bravo on 27/06/21.
+//
+
+import Foundation
---
+++ OnTheMap/Data/DataModel.swift
@@ -0,0 +1,12 @@
+//
+//  DataModel.swift
+//  OnTheMap
+//
+//  Created by Ricardo Bravo on 27/06/21.
+//
+
+import Foundation
+
+class DataModel {
+    static var studentList = [StudentLocationItemResponse]()
+}
