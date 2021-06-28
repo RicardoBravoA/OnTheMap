708068a2c04d3d4e333533e3ab1e23e3aadb71ca
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-27 21:09:56 -0500

    feat: Show web login

--- OnTheMap/Data/Api/EndPoint.swift
+++ OnTheMap/Data/Api/EndPoint.swift
@@ -11,13 +11,15 @@ enum EndPoint {
     static let urlBase = "https://onthemap-api.udacity.com/v1/"
     
     case login
+    case web
     
     var value: String {
         switch self {
-        case .login:
-            return EndPoint.urlBase + "session"
+            case .login:
+                return EndPoint.urlBase + "session"
+            case .web:
+                return "https://auth.udacity.com/sign-in"
         }
-    
     }
     
     var url: URL {
