298f8567f0a1bd0ed4a03261898edc6897742cf5
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-27 21:33:25 -0500

    feat: Add student location response

---
+++ OnTheMap/Model/Response/StudentLocationItemResponse.swift
@@ -0,0 +1,21 @@
+//
+//  StudentLocationItemResponse.swift
+//  OnTheMap
+//
+//  Created by Ricardo Bravo on 27/06/21.
+//
+
+import Foundation
+
+struct StudentLocationItemResponse: Codable {
+    let firstName: String
+    let lastName: String
+    let latitude: Float
+    let longitude: Float
+    let mapString: String
+    let mediaURL: String
+    let uniqueKey: String
+    let objectId: String
+    let createdAt: String
+    let updateAt: String
+}
---
+++ OnTheMap/Model/Response/StudentLocationResponse.swift
@@ -0,0 +1,12 @@
+//
+//  StudentLocationResponse.swift
+//  OnTheMap
+//
+//  Created by Ricardo Bravo on 27/06/21.
+//
+
+import Foundation
+
+struct StudentLocationResponse: Codable {
+    let results: [StudentLocationItemResponse]
+}
