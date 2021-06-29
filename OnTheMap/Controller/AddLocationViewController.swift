a2a116a92430d0f4efdd6be22146e77b8d2a5aff
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-29 00:57:14 -0500

    feat(find location): Add activity indicator

--- OnTheMap/Controller/AddLocationViewController.swift
+++ OnTheMap/Controller/AddLocationViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class AddLocationViewController: UIViewController {
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+    }
+    @IBAction func findLocation(_ sender: UIButton) {
+        if activityIndicator.isAnimating {
+            activityIndicator.stopAnimating()
+        } else {
+            activityIndicator.startAnimating()
+        }
     }
 }
