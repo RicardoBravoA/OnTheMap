e975351df2c5904d49c03deb427fc6755bdaa9b2
Ricardo Bravo <ricardobravo@outlook.com>
2021-06-29 16:56:35 -0500

    feat: Add task for put request

--- OnTheMap/Data/Api/ApiClient.swift
+++ OnTheMap/Data/Api/ApiClient.swift
@@ -128,6 +128,43 @@ class ApiClient {
         task.resume()
     }
     
+    class func taskForPUTRequest<RequestType: Encodable, ResponseType: Decodable>(url: URL, body: RequestType, response: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) {
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "PUT"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        urlRequest.httpBody = try! JSONEncoder().encode(body)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                let response = try decoder.decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(response, nil)
+                }
+            } catch {
+                do {
+                    let errorResponse = try decoder.decode(ErrorResponse.self, from: data)
+                    DispatchQueue.main.async {
+                        completion(nil, errorResponse as Error)
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        completion(nil, error)
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
     private class func resizeData(data: Data, resize: Bool) -> Data {
         var newData: Data
         if resize {
